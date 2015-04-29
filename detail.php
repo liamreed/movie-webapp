e982220038183d061ee58deba500cfaf726f2a95
Liam Reed <liamdreed@gmail.com>
2015-04-29 14:09:11 +0100

    end of day 29/04/2015

--- detail.php
+++ detail.php
@@ -1,52 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/bootstrap.min.js"></script>
-    <?php 
-        $filmID = htmlspecialchars($_GET['id']);
-    ?>
+  <meta charset="utf-8">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/custom.min.css" rel="stylesheet">
+  <?php 
+  $filmID = htmlspecialchars($_GET['id']);
+  ?>
 </head>
- 
+
 <body>
-    <div class="container">
-            <div class="row">
-                <?php 
-                  echo '<h3>Movie ID = ' .$filmID. '</h3>';
-                ?>
-            </div>
-            <div class="row">
-                <p>
-                    <a href="create.php" class="btn btn-success">Create</a>
-                </p>
-                <table class="table table-striped table-bordered">
-                      <thead>
-                        <tr>
-                          <th>Film Name</th>
-                          <th>IMDB Rating</th>
-                          <th>Description</th>
-                          <th>Action</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      <?php
-                       include 'database.php';
-                       $pdo = Database::connect();
-                       $sql = "SELECT * FROM film WHERE FilmId ='".$filmID."'";
-                       foreach ($pdo->query($sql) as $row) {
-                                echo '<tr>';
-                                echo '<td>'. $row['FilmName'] . '</td>';
-                                echo '<td>'. $row['Rating'] . '</td>';
-                                echo '<td>'. $row['Description'] . '</td>';
-                                echo '<td><a class="btn" href="read.php?CustomerId='.$row['CustomerId'].'">Read</a></td>';
-                                echo '</tr>';
-                       }
-                       Database::disconnect();
-                      ?>
-                      </tbody>
-                </table>
-        </div>
-    </div> <!-- /container -->
-  </body>
+  <div class="container">
+    <div class="jumbotron">
+      <div class="container">
+        <?php
+        include 'database.php';
+        $pdo = Database::connect();
+        $sql = "SELECT * FROM film WHERE FilmId ='".$filmID."'";
+        foreach ($pdo->query($sql) as $row) {
+          echo '<h2>'. $row['FilmName'] . '</h2>';
+          echo '<h5>IMDB Rating - '. $row['Rating'] . '/10</h5>';
+          echo '<img class="img-responsive img-cover" src="img/cover/'. $row['FilmId'] .'.jpg" alt="cover"></img>';
+          echo '<p>'. $row['Description'] . '</p>';
+          echo '<a class="btn btn-primary" href="http://www.imdb.com/title/tt'.$row['FilmId'].'">IMDB</a>';
+        }   
+        Database::disconnect();
+        ?>
+      </div>
+    </div>
+    <div class="row">
+      <p>
+        <a href="create.php" class="btn btn-success">Create</a>
+      </p>
+    </div>
+  </div>
+  <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
 </html>
